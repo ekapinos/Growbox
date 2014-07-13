27a292735e375821aa9f254715bf5ada6d997d98
Evgeny Kapinos <evgeny.kapinos@yandex.ru>
2014-07-14 00:30:52 +0300

    refactoring

--- Growbox/Global.cpp
+++ Growbox/Global.cpp
@@ -1,8 +1,13 @@
 #include "Global.h"
 
+/////////////////////////////////////////////////////////////////////
+//                        GLOBAL VARIABLES                         //
+/////////////////////////////////////////////////////////////////////
+
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire g_oneWirePin(ONE_WIRE_PIN);
 
 boolean g_isGrowboxStarted = false;
 byte g_isDayInGrowbox = -1;
-boolean g_useSerialMonitor = false;;
+boolean g_useSerialMonitor = false;
+
